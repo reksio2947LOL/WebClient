df31f5f00df073e09a6e7548f6d361139e1dca4b
JustRex <77068195+reksio2947LOL@users.noreply.github.com>
2021-03-22 18:56:49 +0100

    Poprawiono kilka rzeczy w kodzie

--- webhook.py
+++ webhook.py
@@ -1,19 +1,19 @@
-from discord_webhook import DiscordWebhook
 import time
+from discord_webhook import DiscordWebhook
 from pypresence import Presence
 
 client_id = '823593956461903873'
 RPC = Presence(client_id)
 RPC.connect()
 
-bruh1 = input('Podaj adres webhooka: ')
+webhook_adress = input('Podaj adres webhooka: ')
 
 def webhook_login():
-	RPC.update(state="używając WebClient", details="Piszę na webhooku", large_image="images", large_text="Piszę na webhooku",    start=time.time())
+	RPC.update(state="używając WebClient", details="Piszę na webhooku", large_image="images", large_text="Piszę na webhooku", start=time.time())
 
-	bruh2 = input('Napisz na webhooku: ')
-	webhook = DiscordWebhook(url=f'{bruh1}', content=f'{bruh2}')
+	webhook_message = input('Napisz na webhooku: ')
+	webhook = DiscordWebhook(url=webhook_adress, content=webhook_message)
 	response = webhook.execute()
-	webhook_login()
+	webhook_send()
 
-webhook_login()
+webhook_send()
